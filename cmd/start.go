c5a938304d02eb10844f73bf58ad93dab255919b
Leroy Le <44736156+leroynle@users.noreply.github.com>
2022-12-28 20:53:14 -0800

    first commit

---
+++ cmd/start.go
@@ -0,0 +1,133 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	tcell "github.com/gdamore/tcell/v2"
+	tview "github.com/rivo/tview"
+	"github.com/spf13/cobra"
+)
+
+// startCmd represents the start command
+var startCmd = &cobra.Command{
+	Use:   "start",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		chatGPT3()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(startCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// startCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type Message struct {
+	Timestamp time.Time
+	Username  string
+	Text      string
+}
+
+func chatGPT3() {
+	// c := gogpt.NewClient("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")
+	// ctx := context.Background()
+
+	// result := ""
+
+	// for result != ":exit" {
+	// 	label := emoji.WhiteQuestionMark
+	// 	prompt := promptui.Prompt{
+	// 		Label: label,
+	// 	}
+	// 	res, err := prompt.Run()
+
+	// 	if err != nil {
+	// 		fmt.Printf("Prompt failed %v\n", err)
+	// 		return
+	// 	}
+	// 	result = res
+	// 	fmt.Printf("You answered %s\n", result)
+	// }
+
+	app := tview.NewApplication()
+
+	messages := tview.NewList()
+	inputField := tview.NewInputField()
+	usernameField := tview.NewInputField().SetLabel("Username: ")
+
+	// Set the default username.
+	usernameField.SetText("User")
+
+	// Create a layout that divides the screen into three rows.
+	// Create a layout that divides the screen into three rows.
+	layout := tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(tview.NewBox().SetBorder(true).SetTitle("Messages").AddItem(messages, 0, 1, false), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox().SetBorder(true).AddItem(inputField, 1, 0, false), 1, 0, false).
+			AddItem(tview.NewBox().SetBorder(true).AddItem(inputField, 1, 0, false), 1, 0, false))
+
+	// Handle user input.
+	inputField.SetDoneFunc(func(key tcell.Key) {
+		if key == tcell.KeyEnter {
+			// Get the current timestamp and username.
+			timestampStr := time.Now().Format(time.RFC1123)
+			timestamp, err := time.Parse(time.RFC1123, timestampStr)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			username := usernameField.GetText()
+
+			// Create a new message.
+			message := &Message{
+				Timestamp: timestamp,
+				Username:  username,
+				Text:      inputField.GetText(),
+			}
+
+			// Clear the input field.
+			inputField.SetText("")
+
+			// Add the message to the list.
+			messages.AddItem(fmt.Sprintf("[%s] %s: %s", message.Timestamp, message.Username, message.Text), "", 0, nil)
+			app.SetFocus(messages)
+		}
+	})
+
+	// Start the application.
+	if err := app.SetRoot(layout, true).SetFocus(inputField).Run(); err != nil {
+		fmt.Println(err)
+	}
+
+	// req := gogpt.CompletionRequest{
+	// 	Model:     "text-ada-001",
+	// 	MaxTokens: 500,
+	// 	Prompt:    "Hello, how are you?",
+	// }
+
+	// resp, err := c.CreateCompletion(ctx, req)
+	// if err != nil {
+	// 	return
+	// }
+	// fmt.Println(resp.Choices[0].Text)
+
+}
---
+++ go.mod
@@ -0,0 +1,26 @@
+module chatgpt-cli
+
+go 1.19
+
+require (
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
+	github.com/enescakir/emoji v1.0.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.3 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.9 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/rivo/tview v0.0.0-20221221172820-02e38ea9604c // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/sahilm/fuzzy v0.1.0 // indirect
+	github.com/sashabaranov/go-gpt3 v0.0.0-20221216095610-1c20931ead68 // indirect
+	github.com/spf13/cobra v1.6.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
